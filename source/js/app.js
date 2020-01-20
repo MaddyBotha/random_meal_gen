0d02411affc68387c758eb699c0284cf5ad2f2ca
MaddyBotha <35299442+MaddyBotha@users.noreply.github.com>
2020-01-20 18:27:51 +0200

    added a social panel
    Thanks Florin Pop - I used yours until I add my own 😋

--- source/js/app.js
+++ source/js/app.js
@@ -8,44 +8,58 @@ console.log(mealContainer);
 
 // let's add and event listener so that when we click the button an event should occur
 getMeal.addEventListener("click", () => {
- fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-  .then(res => res.json())
-  .then(res => {
-   createMeal(res.meals[0]);
-  }).catch(e => {
-   console.warn(e);
-  })
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then(res => res.json())
+    .then(res => {
+      createMeal(res.meals[0]);
+    })
+    .catch(e => {
+      console.warn(e);
+    });
 });
-
 
 // let's create the function createMeal from above so that we can make use of it
 // we will use a for loop to loop over the the ingredients in the meal and add an if statement so that if the ingredients add up we add it, otherwise we disgard it
 const createMeal = meal => {
- // we create an empty array so that when we loop over the ingredients, they are added to the array
- const ingredients = [];
+  // we create an empty array so that when we loop over the ingredients, they are added to the array
+  const ingredients = [];
 
- // we can get all the ingredients from the recipe and we'll allow it to go up to 20 items
- for (let i = 1; i <= 20; i++) {
-  if (meal[`strIngredient${i}`]) {
-   ingredients.push(
-    `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
-   );
-  } else {
-   // stop if there are no more ingredients to add
-   break;
+  // we can get all the ingredients from the recipe and we'll allow it to go up to 20 items
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      // stop if there are no more ingredients to add
+      break;
+    }
   }
- }
 
- // let's display the items in html
+  // let's display the items in html
 
- const newInnerHTML = `
+  const newInnerHTML = `
 <div class="grid">
  <div class="feature-card">
   <img src="${meal.strMealThumb}" alt="Meal Image">
 
- 	${meal.strCategory ? `<p class="big"><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
-		${meal.strArea ? `<p class="big"><strong>Area:</strong> ${meal.strArea}</p>` : ''}
-		${meal.strTags ? `<p class="big"><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+ 	${
+    meal.strCategory
+      ? `<p class="big"><strong>Category:</strong> ${meal.strCategory}</p>`
+      : ""
+  }
+		${
+      meal.strArea
+        ? `<p class="big"><strong>Area:</strong> ${meal.strArea}</p>`
+        : ""
+    }
+		${
+      meal.strTags
+        ? `<p class="big"><strong>Tags:</strong> ${meal.strTags
+            .split(",")
+            .join(", ")}</p>`
+        : ""
+    }
 
   <h5 class="big">Ingredients: </h5>
    <ul>
@@ -56,17 +70,32 @@ const createMeal = meal => {
  </div>
 ${
   meal.strYoutube
-   ? `
+    ? `
 </div>
 <div>
  <div class="feature-card flex-video">
   <iframe src="https://www.youtube.com/embed/${meal.strYoutube.slice(
     -11
-   )}" frameborder="0"></iframe>
+  )}" frameborder="0"></iframe>
  </div>
 </div>`
-   : ""
-  } `;
+    : ""
+} `;
 
- mealContainer.innerHTML = newInnerHTML;
+  mealContainer.innerHTML = newInnerHTML;
 };
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector(".floating-btn");
+const close_btn = document.querySelector(".close-btn");
+const social_panel_container = document.querySelector(
+  ".social-panel-container"
+);
+
+floating_btn.addEventListener("click", () => {
+  social_panel_container.classList.toggle("visible");
+});
+
+close_btn.addEventListener("click", () => {
+  social_panel_container.classList.remove("visible");
+});
